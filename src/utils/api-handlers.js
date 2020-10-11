02fe5572f573bcd7450b9e6b5210356703df5cae
wsameer <dev.wsameer@gmail.com>
2020-10-11 11:10:02 +0530

    added more apis

--- src/utils/api-handlers.js
+++ src/utils/api-handlers.js
@@ -1,8 +1,6 @@
 import { SERVER_URL } from '../utils/constants';
 
 async function loginApi({ email, password }) {
-  console.log(`inside api call`);
-  console.log(email, password);
   try {
     const response = await fetch(`${SERVER_URL}/api/login`, {
       method: "post",
@@ -18,8 +16,6 @@ async function loginApi({ email, password }) {
 }
 
 async function registerApi({ email, password }) {
-  console.log(`inside api call`);
-  console.log(email, password);
   try {
     const response = await fetch(`${SERVER_URL}/api/register`, {
       method: "post",
@@ -44,4 +40,44 @@ async function getCustomers(pageNumber) {
   }
 }
 
-export { loginApi, registerApi, getCustomers };
+async function createCustomer(newCustomer) {
+  console.log(newCustomer);
+  const payload = {
+    email: newCustomer.email,
+    name: newCustomer.name
+  };
+  try {
+    const response = await fetch(`${SERVER_URL}/api/users`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
+    return await response.json();
+  } catch (error) {
+    throw Error(error);
+  }
+}
+
+async function updateCustomer(newCustomer, id) {
+  console.log(newCustomer);
+  const payload = {
+    email: newCustomer.email,
+    name: newCustomer.name
+  };
+  try {
+    const response = await fetch(`${SERVER_URL}/api/users/${id}`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
+    return await response.json();
+  } catch (error) {
+    throw Error(error);
+  }
+}
+
+export { loginApi, registerApi, getCustomers, createCustomer, updateCustomer };
