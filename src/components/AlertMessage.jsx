99e24e6c36e2b3119bef1db198bffcd3dbdb24b9
wsameer <dev.wsameer@gmail.com>
2020-10-11 13:30:31 +0530

    renamed errormessage to alert message

---
+++ src/components/AlertMessage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function AlertMessage({ message, type }) {
+  if (!type) {
+    type = 'danger';
+  }
+  return (
+    <div className={`alert alert-${type}`} role="alert">
+      {message}
+    </div>
+  )
+}
+
+AlertMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
+};
+
+export default AlertMessage
+
