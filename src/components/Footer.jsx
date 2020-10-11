4fb6a24ebb204f40060dc6a8e5cf21afe5a74dc7
wsameer <dev.wsameer@gmail.com>
2020-10-11 13:30:02 +0530

    generic footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <nav className="navbar fixed-bottom navbar-light bg-light row text-right">
+      <div className="col-sm-12">
+        <p>Designed & Developed by
+        <a href="https://wsameer.github.io/v1/" target="_blank" rel="noopener noreferrer">Sameer Waskar</a></p>
+      </div>
+    </nav>
+  )
+}
+
+export default Footer;
